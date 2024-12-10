f5b29f5c90e8b429e6cf60b599bce74c80fc2fa9
Sirdisalot <sirdisalot@gmail.com>
2024-12-10 13:50:22 +0200

    Done

--- src/copystatic.py
+++ src/copystatic.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 from markdown_block import markdown_to_html_node
+from pathlib import Path
 
 def copy_file_recursive(source_dir_path, dest_dir_path):
     if not os.path.exists(dest_dir_path):
@@ -40,3 +41,13 @@ def generate_page(from_path, template_path, dest_path):
         os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     to_file = open(dest_path, "w")
     to_file.write(template)
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        if os.path.isfile(from_path):
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(from_path, template_path, dest_path)
+        else:
+            generate_pages_recursive(from_path, template_path, dest_path)
--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 
-from copystatic import copy_file_recursive, generate_page
+from copystatic import copy_file_recursive, generate_pages_recursive
 
 dir_path_static = "./static"
 dir_path_public = "./public"
@@ -9,15 +9,13 @@ dir_path_content = "./content"
 template_path = "./template.html"
 
 def main():
+    print("Deleting public directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
+    print("Copying static files to public directory...")
     copy_file_recursive(dir_path_static, dir_path_public)
 
     print("Generating page...")
-    generate_page(
-        os.path.join(dir_path_content, "index.md"),
-        template_path,
-        os.path.join(dir_path_public, "index.html"),
-    )
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
 
 main()
